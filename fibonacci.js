2b977d1194cf16acea1bd6bc8d3f027406e418eb
Zuby128 <zberber20@gmail.com>
2021-03-21 15:55:33 +0300

    jsworkouts fibonacci and reverse added

---
+++ fibonacci.js
@@ -0,0 +1,24 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-plusplus */
+/* eslint-disable prefer-const */
+
+// fibonacci numbers
+let fibonacci = [];
+fibonacci[0] = 1;
+fibonacci[1] = 1;
+for (let i = 2; i < 100; i++) {
+  fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+}
+
+console.log(fibonacci[7]);
+console.log(fibonacci[12]);
+
+// reverse string
+const text = 'something';
+let reverseText = '';
+for (let i = text.length - 1; i >= 0; i--) {
+  reverseText += text[i];
+}
+console.log(reverseText);
